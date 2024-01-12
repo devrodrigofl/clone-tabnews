3bd3d93340ddd552977b2f5073b48757abcf0f5f
Rodrigo Lanza <96920312+devrodrigofl@users.noreply.github.com>
2024-01-12 22:19:09 +0000

    altera a frase para minha mãe

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Jéssica, meu amor, eu amo você! Esse é meu primeiro trabalho e vim te surpreender, se você gostou, não há de quê! Se você me ama, da uma risadinha e logo depois uma beijoquinha!❤️</h1>
+    return <h1>Mãe, esse é meu primeiro trabalho e vim te surpreender, obrigado por sempre me apoiar eu amo você!❤️ Se você gostou da uma risadinha!</h1>
 }
 
 export default Home;
