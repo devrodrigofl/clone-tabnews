0d0a068ba91ced7c3aa98d7793d5a30b5ec45753
Rodrigo Lanza <96920312+devrodrigofl@users.noreply.github.com>
2024-01-12 18:53:15 +0000

    adiciona o arquivo pages

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Jéssica, meu amor, eu amo você! Esse é meu primeiro trabalho e vim te surpreender, se você gostou, não há de quê! Se você me ama, da uma risadinha e logo depois uma beijoquinha!</h1>
+}
+
+export default Home;
