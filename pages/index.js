5e59e015c7fd5a270fb8581b24ff7a95c0a88197
Rodrigo Lanza <96920312+devrodrigofl@users.noreply.github.com>
2024-11-27 22:41:01 +0000

    voltando pagina principal para teste

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Jéssica, meu amor, eu amo você! Esse é meu primeiro trabalho e vim te
-      surpreender, se você gostou, não há de quê! Se você me ama, da uma
-      risadinha e logo depois uma beijoquinha!❤️
-    </h1>
-  );
+  return <h1>teste</h1>;
 }
 
 function teste() {
